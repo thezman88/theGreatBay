e0768733d218059e252a7d8af235eee13cdd2125
Nicholas Buck <njibuck@gmail.com>
2019-10-19 13:50:34 -0400

    Minor updates

--- GreatBayPrompts.js
+++ GreatBayPrompts.js
@@ -4,9 +4,6 @@ const inquirer = require('inquirer');
 
 // Reference source code files
 // let movieSearch = require("./call_omdb.js");
-// let bandSearch = require("./call_bandsintown.js");
-// let songSearch = require("./call_spotify.js");
-// let random = require("./call_random.js");
 
 // Create a "Prompt" with a series of questions.
 function initialQuestion() {
@@ -18,13 +15,6 @@ inquirer
             name: 'actionType',
             choices: ['1. Post an item', '2. Bid on an item'],
         }
-        // ,
-        // // Here we create a basic text prompt.
-        // {
-        //     type: 'input',
-        //     message: 'Input your search / request',
-        //     name: 'input',
-        // }
     ])
     .then(function (inquirerResponse) {
         switch (inquirerResponse.actionType) {
